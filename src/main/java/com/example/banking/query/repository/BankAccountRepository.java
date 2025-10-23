1b5adc38c98fe0e0c05315ac7f2c3636b6129f17
josmellbu <josmell.alex@gmail.com>
2025-10-23 15:26:24 -0500

    feat: clean code

--- src/main/java/com/example/banking/query/repository/BankAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.banking.query.repository;
-
-import com.example.banking.query.entity.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-}
